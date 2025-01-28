18bae8b45bed1c2067a4ea39b1755acc28938936
Cjay94 <cjephthah94@gmail.com>
2025-01-28 18:44:29 +0200

    searchbar functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import { CarCard, CustomFilter, SearchBar } from "@/components";
 import Hero from "@/components/Hero";
+import { FilterProps } from "@/types";
 import { fetchCars } from "@/utils";
 import { Fragment } from "react";
 
-export default async function Home() {
-    const allCars = await fetchCars()
+interface HomeProps {
+    searchParams: FilterProps
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+    const allCars = await fetchCars({
+        manufacturer: searchParams.manufacturer || '',
+        year: searchParams.year || 2022,
+        fuel: searchParams.fuel || '',
+        limit: searchParams.limit || 10,
+        model: searchParams.model || '',
+    })
+
 
     const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
 
